e5dac76e5fde524cfa404e3b2be60aeb928215d4
benjamin-abebe <benjamin.abebe@stu.ocsb.ca>
2025-06-05 17:00:47 +0000

    init

--- answer.php
+++ answer.php
@@ -28,16 +28,49 @@
         <img src="./bible.jpg" alt="Bible" />
         <p class="page-content">Click the button below to see a Bible verse!</p>
 
-        <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
-          type="submit">
+        <a class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
+          href="./index.php" type="button">
           Another Bible Verse
-        </button>
+        </a>
 
         <br /><br />
+          <?php
+          // Randomly pick a verse ID
+          $verses = ["JHN.3.16", "PSA.23.1", "ROM.8.28", "MAT.6.33", "PHI.4.13"];
+          $randomVerseId = $verses[array_rand($verses)];
 
-        <div class="page-content-answer">
-          <div id="verse" style="font-size: 1.2em; padding: 1em;"></div>
-        </div>
+          $apiKey = "ac9bf9be9a365d3bfec89c6837e17f1a";
+          $bibleId = "de4e12af7f28f599-02"; 
+
+          // API URL
+          $url = "https://api.scripture.api.bible/v1/bibles/$bibleId/verses/$randomVerseId";
+
+          $options = [
+            "http" => [
+              "method" => "GET",
+              "header" => "api-key: $apiKey\r\n"
+            ]
+          ];
+
+          $context = stream_context_create($options);
+
+          // Make the request
+          $response = file_get_contents($url, false, $context);
+
+          // Decode the JSON
+          if ($response !== false) {
+            $data = json_decode($response, true);
+            $reference = $data["data"]["reference"];
+            $content = $data["data"]["content"];
+          } else {
+            $reference = "Error";
+            $content = "Could not fetch verse.";
+          }
+          ?>
+
+          <div class="page-content-answer">
+            <div id="verse" ></div>
+          </div>
       </div>
     </main>
     </form>
